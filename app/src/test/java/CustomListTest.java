78b8765de38244600f6319670cde8d29d002936c
Lizzy Barron <ebarron@ualberta.ca>
2021-11-01 00:35:40 -0600

    test cases

--- app/src/test/java/CustomListTest.java
+++ app/src/test/java/CustomListTest.java
@@ -1,10 +1,13 @@
-import static org.junit.Assert.assertEquals;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.example.simpleparadox.listycity.City;
 import com.example.simpleparadox.listycity.CustomList;
 
+
 import org.junit.Before;
-import org.junit.Test;
+import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 
@@ -18,7 +21,11 @@ public class CustomListTest {
     {
         list = new CustomList(null, new ArrayList<City>());
     }
-
+    private City mockCity()
+    {
+        City city = new City("Calgary", "Alberta");
+        return city;
+    }
 
     @Test
     public void addCityTest()
@@ -28,4 +35,41 @@ public class CustomListTest {
         list.addCity(c);
         assertEquals(1, list.getCount());
     }
+
+    @Test
+    public void testHasCity()
+    {
+
+        City city = new City("Calgary", "Alberta");
+        list.addCity(city);
+        boolean testBool = list.hasCity(city);
+
+        assertEquals(testBool, true);
+    }
+    @Test
+    public void testGetCities()
+    {
+
+        City city = new City("Calgary", "Alberta");
+        list.add(city);
+
+        assertEquals(city, mockCity());
+    }
+
+
+
+    @Test
+    public void testDelete()
+    {
+        City city1 = new City("Calgary", "Alberta");
+        City city2 = new City("Edmonton", "Alberta");
+        list.addCity(city1);
+        list.add(city2);
+        list.delete(city1);
+        assertEquals(1, list.getCities().size());
+
+
+    }
+
+
 }
